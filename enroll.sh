c41649717d15c428cfc78946ee1fa84a068b97bf
Emerson Jacobson <emwjacobson@gmail.com>
2025-02-23 13:58:23 -0800

    Added unknown distro case

--- enroll.sh
+++ enroll.sh
@@ -21,6 +21,9 @@ if [[ "${distro}" == "debian" ]]; then
   sudo apt install -y curl wget sudo libuser
 elif [[ "${distro}" == "rhel" ]]; then
   sudo dnf install -y curl wget 
+else
+  echo "Unsupported distro"
+  exit 1
 fi
 
 KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0pt323arG58oeGHqvGIeqyn7ksyml7+MVcKohCYtOBuqlsF3l2Vb9NbKYz91kKjdBHzfCFt3uabDewRRsCjdf/pjhIh0XL0LxTl4dwnZj7QHpyBDOY9qlmibkBjJOBaIijPD8QZL6IuxdNfCrVkOrjoDpMWM6DPXn75iXGV0P1GkJQ602joNjsKBjN2O2mYUHZLIQawHlPt6gnInvPPWIenO/ImPP8+n/m0URadGivOaJaTBTSZ8bB29CXdvSd72fP/6oJuAcKJVl2VL68kawBKekh9Rurg8hAopoVqCnW+pgJEsdcbMpU4K/hbUYebztoQ/dPhM8wlpqt6ES4weduCH5nBRu//xi/J4V1a/VykvCZyM6OexyHPeYWzOOkaeHq+AleMBe7ZL6TnAHe+dy1BAHqNjljgpzeUVqHP9Q3NL2Btj/GnGy/+fWjeAx8s1xyQWRPiej79WUAp8CwSEi2c0V0cTpHqio/V52z02Vzn+/cDsBghHRphTMzMEDpxs= ansible@docker"
